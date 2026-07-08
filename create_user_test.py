0801bb90b790e52da16a9ab731cacdf8163355f2
IsmaelBR <ibenitezrenteria@gmail.com>
2026-07-08 10:06:00 -0600

    Mi segundo commit

--- create_user_test.py
+++ create_user_test.py
@@ -1,7 +1,6 @@
 import sender_stand_request
 import data
 
-# esta función cambia los valores en el parámetro "firstName"
 def get_user_body(first_name):
     current_body = data.user_body.copy()
     current_body["firstName"] = first_name
@@ -47,8 +46,8 @@ def test_create_user_1_letter_in_first_name_get_error_response():
 def test_create_user_16_letter_in_first_name_get_error_response():
     negative_assert_symbol("IsmaelBenitezRen")
 
-#def test_create_user_has_space_in_first_name_get_error_response():
-    #negative_assert_symbol("J Simpson")
+def test_create_user_has_space_in_first_name_get_error_response():
+    negative_assert_symbol("J Simpson")
 
 def test_create_user_has_special_symbol_in_first_name_get_error_response():
     negative_assert_symbol("\"№%@\",")
